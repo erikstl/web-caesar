4e9db4078843241b84b01a83026a8318c44ac09f
Erik Bates <erik.s.bates@gmail.com>
2017-03-23 20:13:16 -0500

    Formatting messages; Text box resize; Custom rotation

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ page_header = """
     <title>Web Caesar</title>
 </head>
 <body>
-    <h1> Web Caesar</h1>
+    <h1>Web Caesar</h1>
 """
 
 # html boilerplate for the bottom of every page
@@ -22,12 +22,29 @@ page_footer = """
 class Index(webapp2.RequestHandler):
 
     def get(self):
-        message = "Hello World!"
-        encrypted_message = caesar.encrypt(message, 13)
+        content = """
+            <form action="/encrypt" method="post">
+                <label><p>Enter text to encrypt:</p>
+                <textarea rows="4" cols="50" name="secret-message"></textarea>
+                <p>Rotation factor:</p>
+                <input type="text" name="rotation"/>
+                <input type="submit" value="Encrypt!"/>
+            </form>
+        """
+        self.response.write(page_header + content + page_footer)
 
-        self.response.write(page_header + encrypted_message + page_footer)
+class CaesarEncrypt(webapp2.RequestHandler):
+    def post(self):
+        #message = "Hello World!"
+        message = self.request.get("secret-message")
+        rot = self.request.get("rotation")
+        encrypted_message = caesar.encrypt(message, rot)
+        content = page_header + "<p>Your encrypted message:</p>" + encrypted_message + page_footer
+
+        self.response.write(content)
 
 
 app = webapp2.WSGIApplication([
-    ('/', Index)
+    ('/', Index),
+    ('/encrypt', CaesarEncrypt)
 ], debug=True)
